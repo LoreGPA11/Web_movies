0d02fbc0a2b6981034bbd6ed0bb908d0b205d39e
Lorena Pelayo Arreola <lorena.pelayo@udgvirtual.udg.mx>
2025-11-08 21:33:47 -0600

    Conectar con PostgreSQL en Render_intento_5

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const apiRoutes = require('/api');
+const apiRoutes = require('./api');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
